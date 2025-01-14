57deb492adac4a0a6c087bd6da127f4babddf3a9
Belka <vfrosenyuk@gmail.com>
2025-01-14 15:43:20 +0400

    added sttyles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,14 @@
+<script setup>
+import AppLayout from "@/layouts/AppLayout.vue";
+import HomeView from "@/views/HomeView.vue";
+</script>
+
 <template>
   <app-layout>
     <home-view />
   </app-layout>
 </template>
 
-<script setup>
-import AppLayout from "@/layouts/AppLayout.vue";
-import HomeView from "@/views/HomeView.vue";
-</script>
+<style lang="scss">
+@import "@/assets/scss/app.scss";
+</style>
