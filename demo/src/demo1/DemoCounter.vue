2b92b93d94e1e77d8620782f5069b89c78da408c
Belka <vfrosenyuk@gmail.com>
2025-01-21 16:44:17 +0400

    refactoring demo-counter

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,35 @@
+<script setup>
+import CounterValue from './CounterValue.vue'
+import CounterButton from './CounterButton.vue'
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const decrement = () => {
+  count.value = count.value - 1
+}
+
+const increment = () => {
+  count.value = count.value + 1
+}
+</script>
+
+<template>
+  <demo-container>
+    <div class="counter">
+      <counter-button @action="decrement">-</counter-button>
+
+      <counter-value :value="count" />
+
+      <counter-button @action="increment">+</counter-button>
+    </div>
+  </demo-container>
+</template>
+
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
